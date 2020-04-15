e979159cd27032309370e43ef2c05c7283522891
RyanStonebraker <ryanastonebraker@gmail.com>
2020-04-15 01:01:24 -0800

    Adds working discord bot and 124M model for harry potter

--- discord_bot/bot.py
+++ discord_bot/bot.py
@@ -1,24 +1,97 @@
 import os
 import discord
+import contextlib
 
 from dotenv import load_dotenv
+
+import generator
+import pandas as pd
+
+# characters = ["arsh", "bradley", "percy", "charlie", "neville", "fred", "george", "molly", "percival graves", "lucas", "flinch", "grindelwald", "dobby", "cedric", "myrtle", "harry", "ron", "hermione", "snape", "albus dumbledore", "dumbledore", "tom riddle", "hagrid", "environment", "voldemort", "malfoy", "draco", "mcgonagall", "lupin", "sirius", "hedwig", "headless nick", "dudley", "vernon", "arthur", "ginny", "crab", "goyle", "voldemort"]
+
+# characters = ["arsh", "bradley", "nathan", "addeline", "tristan", "ryan", "george", "andrew", "matt", "ian", "izac", "jason", "tyler", "harry", "hermione", "ron", "tyler"]
+
+characters = {
+    "arsh": "Van Arsh",
+    "bradley": "Bradley Morton",
+    "nathan": "Nathan VanOverbeke",
+    "addeline": "Addeline",
+    "tristan": "Tristan Van Cise",
+    "ryan": "Ryan Van Stonebraker",
+    "george": "george",
+    "matt": "Matt Perry",
+    "tyler": "Tyler Chase",
+    "harry": "harry",
+    "hermione": "hermione",
+    "ron": "ron",
+    "andrew": "Andrew Van Adler",
+    "trump": "Donald Julio Trump",
+    "izac": "Izac Lorimer"
+}
+
+lucidDream = generator.LucidDream(
+    characters=list(characters.keys()).copy(),
+    response_length=40
+)
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD-BOT-LD-TOKEN')
 
 client = discord.Client()
 
+save_counter = {}
+
+async def write_history(message):
+    with open(f"history/{message.guild.name}.txt", "w") as discord_writer:
+            async for message in message.channel.history(limit=100000):
+                discord_writer.write(f"{message.author.display_name}: {message.content}\n")
+
 @client.event
 async def on_message(message):
+    global save_counter
     if message.author == client.user:
         return
 
-    if message.content.startswith('!hello'):
-        msg = 'Hello {0.author.mention}'.format(message)
-        await message.channel.send(msg)
-    elif message.content.startswith("!history"):
-        async for history_message in message.channel.history():
-            print(history_message.content)
+    save_counter[message.guild.name] = 1 if message.guild.name not in save_counter else save_counter[message.guild.name] + 1
+
+    amended_characters = {guild_member.display_name: guild_member.display_name for guild_member in message.guild.members}
+    amended_characters.update(characters)
+
+    if message.content.startswith("!save"):
+        await write_history(message)
+        return
+    elif message.content.startswith("!breakdown"):
+        character = message.content.replace("!breakdown", "").strip()
+        await message.guild.me.edit(nick=character)
+        await message.channel.send(lucidDream.get_emotional_profile(character, message.guild.name))
+        return
+
+    for character, real_character in amended_characters.items():
+        if character.lower() in message.content.lower():
+            history = await message.channel.history(limit=50).map(lambda old_message: (old_message.author.display_name, old_message.content)).flatten()
+            # await message.channel.send(f"{character}: let me think...")
+            await message.guild.me.edit(nick=real_character)
+            lucidDream.characters = [real_character]
+
+            # channel_history = await message.channel.history(limit=31).flatten()
+            # print("CHANNEL HISTORY", channel_history)
+            print(f"{real_character} is responding")
+            async with message.channel.typing():
+                response = lucidDream.start_conversation(history, filtered=True)
+
+                for member in message.guild.members:
+                    response = response.replace(member.display_name, member.mention) if member.display_name in response else response
+                        # print(member.nick, member.display_name)
+                # for _, character in characters.items():
+                #     if character in response:
+                #         member_mention = discord.utils.get(message.guild.members, name=character)
+                #         member_mention = member_mention.mention if member_mention else character
+                #         response = response.replace(character, member_mention)
+                await message.channel.send(response)
+
+    if save_counter[message.guild.name] > 5:
+        save_counter[message.guild.name] = 0
+        await write_history(message)
 
 @client.event
 async def on_ready():
---
+++ discord_bot/generator.py
@@ -0,0 +1,172 @@
+import sys
+import os
+import contextlib
+with open(os.devnull, "w") as f, contextlib.redirect_stdout(f):
+    os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+    import gpt_2_simple as gpt2
+    import tensorflow as tf
+    tf.logging.set_verbosity(tf.logging.ERROR)
+    tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+    import logging
+    logging.getLogger('tensorflow').setLevel(logging.FATAL)
+
+import random
+import pandas as pd
+import numpy as np
+import math
+import re
+
+from nltk.corpus import stopwords
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.linear_model import SGDClassifier
+from sklearn.model_selection import train_test_split
+
+
+class LucidDream:
+    def __init__(
+        self,
+        characters=["trump", "harry", "ron", "hermione", "snape", "albus dumbledore", "tom riddle", "hagrid", "environment"],
+        response_length=30
+    ):
+        self.characters = characters
+        self.sess = None
+        self.response_length=response_length
+
+        self.init_classifier()
+
+
+    def init_classifier(self):
+        isear_df = pd.read_csv("../corpora/isear.csv", header=None)
+        isear_df.columns = ["emotion", "text", ""]
+        isear_df = isear_df.drop([""], axis=1)
+
+        cleaned_text = [self.clean_text(text) for text in isear_df["text"].tolist()]
+
+        count_vectorizer = CountVectorizer()
+        training_counts = count_vectorizer.fit_transform(cleaned_text)
+        tfidf_transformer = TfidfTransformer()
+        bag_of_words = count_vectorizer.transform(cleaned_text)
+        tfidf_transformer.fit(bag_of_words)
+
+        self.classifier = Pipeline([
+                ('vect', count_vectorizer), 
+                ('tfidf', tfidf_transformer),
+                ('clf', SGDClassifier(loss="log", 
+                                    penalty='l1',
+                                    random_state=1
+                                    ))
+            ])
+
+        X_train, X_test, Y_train, Y_test = train_test_split(cleaned_text, isear_df["emotion"].tolist(), test_size=0.3, random_state=1)
+        self.classifier.fit(X_train, Y_train)
+
+
+    def clean_text(self, content):
+        content = content.lower().strip()
+        content = re.sub(r"[^a-zA-Z]", " ", str(content))
+        content = re.sub(r"[\s\t\n]+", " ", content)
+        tokens = [word for word in content.split() if word and word not in stopwords.words("english")]
+        cleaned_text = " ".join(tokens)
+        return cleaned_text
+
+
+    def generate_holistic_model_response(self, conversation, character, filtered=True):
+        common_letters = "tainosrtainosiatwbmtikgdpr"
+        seed = "\n".join([f"{sentence[0].strip()}: {sentence[1].strip()}" for sentence in conversation[-30::-1]]) + f"\n{character}:"
+        # print(f"Seed: {seed}")
+        self.sess = gpt2.start_tf_sess()
+        gpt2.load_gpt2(self.sess, run_name=f"csc_model")
+        response = gpt2.generate(
+            self.sess,
+            length=self.response_length,
+            temperature=0.7,
+            prefix=seed + random.choice(common_letters + common_letters.upper()),
+            nsamples=1,
+            batch_size=1,
+            run_name=f"csc_model",
+            return_as_list=True
+        )[0]
+        print("RAW:\n", response[len(seed):])
+        response = response[len(seed):].strip()
+        # print("R", response)
+        
+        # if len(response.split(":")) > 1:
+        #     response = response.split(":")[0].strip()
+        # filtered_words = " ".join(re.findall(r".+?[.?!]", response)).strip()
+
+        gpt2.reset_session(self.sess)
+        self.sess = gpt2.start_tf_sess()
+
+        # print("R", response, len(re.split(r"[\s\t\n]", response)) - len(re.split(r"[\s\t\n]", filtered_words)))
+
+        # return response
+
+        # filtered_response = filtered_words if len(re.split(r"[\s\t\n]", response)) - len(re.split(r"[\s\t\n]", filtered_words)) <= 3 and filtered_words.strip() else response
+        # print("FR", filtered_response)
+        return "\n".join(response.split("\n")[:1])
+        # return filtered_response
+        # return re.split(r"[a-z A-Z0-9]+:", response)[0].strip().split("\n")[0] if filtered else response
+
+
+    def start_conversation(
+        self,
+        conversation=[],
+        filtered=True
+    ):
+        # with open(os.devnull, "w") as f, contextlib.redirect_stdout(f):
+        character = random.choice(self.characters)
+        response = self.generate_holistic_model_response(conversation, character, filtered=filtered)
+        return response
+
+
+    def get_class_probs(self, data):
+        prob_spread = self.classifier.predict_proba([data]).tolist()[0]
+        probabilities = pd.DataFrame({"class": self.classifier.classes_, "probability": prob_spread})
+        probabilities = probabilities.sort_values(by="probability", ascending=False)
+        probabilities = probabilities.set_index("class").T.reset_index()
+        del probabilities["index"]
+        probabilities["predicted"] = self.classifier.predict([data])
+        return probabilities
+
+    
+    def get_emotional_profile(self, character, guild):
+        history = {"character": [], "text": []}
+        with open (f"history/{guild}.txt", "r") as history_reader:
+            raw_lines = history_reader.readlines()
+            for line in raw_lines:
+                history["character"].append(line[:line.find(":")])
+                history["text"].append(line[line.find(":")+1:].strip())
+        df = pd.DataFrame(history)
+
+
+        emotion_breakdown = pd.concat([self.get_class_probs(row.text) for i, row in df[df.character == character].iterrows()], sort=False)
+
+        character_df = pd.DataFrame(emotion_breakdown.mean()).T
+        character_df.index.name = character
+        character_df = character_df.T.rename(columns={0:"percent"}).sort_values(by="percent", ascending=False)
+        return character_df
+
+
+if __name__ == "__main__":
+    # import gpt_2_simple as gpt2
+    # sess = gpt2.start_tf_sess()
+    # gpt2.finetune(
+    #     sess,
+    #     "../corpora/harry_potter/small_text.txt",
+    #     model_name="124M",
+    #     steps=100,
+    #     restore_from="fresh",
+    #     run_name="small_text_small_run1",
+    #     print_every=10,
+    #     sample_every=200,
+    #     save_every=100
+    # )
+
+
+    lucidDream = LucidDream(
+        characters=["harry", "ron", "hermione", "snape", "albus dumbledore", "tom riddle", "hagrid", "environment"],
+        response_length=50
+    )
+
+    print(lucidDream.start_conversation([("harry", "this is a test.")], filtered=True))
